ea74c3f2f0cc20cee6952c798c84387ac7f02e7a
Matheus Gabriel <102601656+theusgab@users.noreply.github.com>
2023-05-12 18:20:40 -0300

    Add files via upload

---
+++ js/paginar.js
@@ -0,0 +1,14 @@
+function Pag1() {
+    document.querySelector('.pagina.active').classList.remove('active');
+    document.querySelectorAll('.pagina')[0].classList.add('active');
+}
+
+function Pag2() {
+    document.querySelector('.pagina.active').classList.remove('active');
+    document.querySelectorAll('.pagina')[1].classList.add('active');
+}
+
+function Pag3() {
+    document.querySelector('.pagina.active').classList.remove('active');
+    document.querySelectorAll('.pagina')[2].classList.add('active');
+}
